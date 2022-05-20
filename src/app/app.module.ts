1bcfd09c98b64a8cf92044ca8f74bb09889daf1c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-20 13:30:50 -0300

    Update

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { IconsComponent } from './icons/icons.component';
+import { ProductsComponent } from './products/products.component';
+import { ReviewComponent } from './review/review.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    IconsComponent,
+    ProductsComponent,
+    ReviewComponent,
+    ContactsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
